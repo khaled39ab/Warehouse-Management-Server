a0fa24572385da23a40f71075eaf306f7015a1c6
khaled39ab <khaled39ab@gmail.com>
2023-03-16 15:30:41 +0600

    successfully get items

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 4000;
 
 const app = express();
@@ -7,10 +9,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) =>{
+
+// const uri = `mongodb+srv://${process.env.WAREHOUSE_USER}:${process.env.WAREHOUSE_PASSWORD}@cluster0.qudsyns.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.WAREHOUSE_USER}:${process.env.WAREHOUSE_PASSWORD}@cluster0.uwlfrmd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    const itemsCollection = client.db("WarehouseManagement").collection("Items");
+
+    /* 
+        ================================================================================
+        +++++++++++++++++++++++++++++++   Items Section  +++++++++++++++++++++++++++++++
+        ================================================================================
+    */
+
+        app.get('/items', async(req, res) =>{
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send("Hello from warehouse management server")
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log("server is running from", port);
 });
